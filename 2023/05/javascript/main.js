a615123d285e4161056758f204d5545f94c9337e
Luj8n <luj8n@duck.com>
2023-12-04 23:24:51 +0100

    fixed day names

---
+++ 2023/05/javascript/main.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("../input.txt").toString().trim();
+
+console.log(`Part 1: ${part1()}`);
+console.log(`Part 2: ${part2()}`);
+
+function part1() {}
+
+function part2() {}
